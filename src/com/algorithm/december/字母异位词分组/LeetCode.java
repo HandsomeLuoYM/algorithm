5978877e0f84e61dd1cd24f45c428e1f2fd2462a
LuoYM-Warehouse <git2224357949@qq.com>
2020-12-27 01:30:27 +0800

    12/27 阶段性提交

---
+++ src/com/algorithm/december/字母异位词分组/LeetCode.java
@@ -0,0 +1,66 @@
+package com.algorithm.december.字母异位词分组;
+
+import java.util.*;
+
+/**
+ * @author Ming
+ * @date 2020/12/14 - 1:17
+ * @describe
+ */
+public class LeetCode {
+
+    /**
+     * 给定一个字符串数组，将字母异位词组合在一起。字母异位词指字母相同，但排列不同的字符串。
+     *
+     * 输入: ["eat", "tea", "tan", "ate", "nat", "bat"]
+     * 输出:
+     * [
+     *   ["ate","eat","tea"],
+     *   ["nat","tan"],
+     *   ["bat"]
+     * ]
+     *
+     */
+    /**
+     * 自己的做法：
+     *      和题解二一样，统计个数用hash存储（计数存储）
+     * 执行用时：22 ms, 在所有 Java 提交中击败了21.42%的用户
+     * 内存消耗：43 MB, 在所有 Java 提交中击败了5.53%的用户
+     */
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String,List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            int length = str.length();
+            int[] nums = new int[26];
+            for (int i = 0; i<length; i++){
+                nums[str.charAt(i)-'a']++;
+            }
+            String key = Arrays.toString(nums);
+            if (map.containsKey(key)){
+                map.get(key).add(str);
+            }else {
+                map.put(key,new ArrayList<String>(){{add(str);}});
+            }
+        }
+        return new ArrayList<List<String>>(map.values());
+    }
+
+    /**
+     * 排序存储
+     */
+    class Solution {
+        public List<List<String>> groupAnagrams(String[] strs) {
+            Map<String, List<String>> map = new HashMap<String, List<String>>();
+            for (String str : strs) {
+                char[] array = str.toCharArray();
+                Arrays.sort(array);
+                String key = new String(array);
+                List<String> list = map.getOrDefault(key, new ArrayList<String>());
+                list.add(str);
+                map.put(key, list);
+            }
+            return new ArrayList<List<String>>(map.values());
+        }
+    }
+
+}
--- src/com/algorithm/december/摆动序列/LeetCode.java
+++ src/com/algorithm/december/摆动序列/LeetCode.java
@@ -18,7 +18,102 @@ public class LeetCode {
      * 通过从原始序列中删除一些（也可以不删除）元素来获得子序列，剩下的元素保持其原始顺序。
      *
      */
+    /**
+     * 自己的思路（贪心）：找到峰
+     * 执行用时：0 ms, 在所有 Java 提交中击败了100.00%的用户
+     * 内存消耗：36.2 MB, 在所有 Java 提交中击败了46.04%的用户
+     */
     public int wiggleMaxLength(int[] nums) {
-
+        int n = nums.length;
+        if (n < 2) {
+            return n;
+        }
+        int prevdiff = nums[1] - nums[0];
+        int ret = prevdiff != 0 ? 2 : 1;
+        for (int i = 2; i < n; i++) {
+            int diff = nums[i] - nums[i - 1];
+            if ((diff > 0 && prevdiff <= 0) || (diff < 0 && prevdiff >= 0)) {
+                ret++;
+                prevdiff = diff;
+            }
+        }
+        return ret;
     }
+
+    /**
+     * 自己的做法：
+     *      对于边界和目前位置定位不准确
+     * 执行用时：0 ms, 在所有 Java 提交中击败了100.00%的用户
+     * 内存消耗：35.8 MB, 在所有 Java 提交中击败了85.28%的用户
+     */
+    public int wiggleMaxLength1(int[] nums) {
+        int all = 1, length = nums.length, j, i=1;
+        if (length<=1) return length;
+        boolean flag = true;
+        while (i<length && nums[i-1]==nums[i]) i++;
+        if (i<length && nums[i-1]>nums[i]) flag = false;
+        while (i<length){
+            if (flag){
+                while (i<length && nums[i]>=nums[i-1]) i++;
+                all++;
+                flag = false;
+            }else {
+                while (i<length && nums[i]<=nums[i-1]) i++;
+                all++;
+                flag = true;
+            }
+        }
+        return all;
+    }
+
+    /**
+     * 动态规划
+     */
+    class Solution {
+        public int wiggleMaxLength(int[] nums) {
+            int n = nums.length;
+            if (n < 2) {
+                return n;
+            }
+            int[] up = new int[n];
+            int[] down = new int[n];
+            up[0] = down[0] = 1;
+            for (int i = 1; i < n; i++) {
+                if (nums[i] > nums[i - 1]) {
+                    up[i] = Math.max(up[i - 1], down[i - 1] + 1);
+                    down[i] = down[i - 1];
+                } else if (nums[i] < nums[i - 1]) {
+                    up[i] = up[i - 1];
+                    down[i] = Math.max(up[i - 1] + 1, down[i - 1]);
+                } else {
+                    up[i] = up[i - 1];
+                    down[i] = down[i - 1];
+                }
+            }
+            return Math.max(up[n - 1], down[n - 1]);
+        }
+    }
+
+
+    /**
+     * 动态规划二
+     */
+    class Solution1 {
+        public int wiggleMaxLength(int[] nums) {
+            int n = nums.length;
+            if (n < 2) {
+                return n;
+            }
+            int up = 1, down = 1;
+            for (int i = 1; i < n; i++) {
+                if (nums[i] > nums[i - 1]) {
+                    up = Math.max(up, down + 1);
+                } else if (nums[i] < nums[i - 1]) {
+                    down = Math.max(up + 1, down);
+                }
+            }
+            return Math.max(up, down);
+        }
+    }
+
 }
